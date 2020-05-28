99d5c26941141020c507d6216bdb09a72e7ccbd8
Jordan Castelloe <jordan.castelloe@gmail.com>
2020-05-28 11:32:06 -0400

    printing from json server

--- src/components/animal/AnimalCard.js
+++ src/components/animal/AnimalCard.js
@@ -1,16 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class AnimalCard extends Component {
   render() {
-    console.log("You're looking at the animals page!")
+    // console.log("this is this.props", this.props);
+    // console.log("this is this.props.animalProp", this.props.animalProp);
     return (
       <div className="card">
         <div className="card-content">
           <picture>
-            <img src={require('./dog.svg')} alt="My Dog" />
+            <img src={require("./dog.svg")} alt="My Dog" />
           </picture>
-          <h3>Name: <span className="card-petname">Doodles</span></h3>
-          <p>Breed: Poodle</p>
+          <h3>
+            Name:{" "}
+            <span className="card-petname">{this.props.animal.name}</span>
+          </h3>
+          <p>Breed: {this.props.animal.breed}</p>
         </div>
       </div>
     );
--- src/components/animal/AnimalList.js
+++ src/components/animal/AnimalList.js
@@ -1,35 +1,37 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 //import the components we will need
-import AnimalCard from './AnimalCard'
-import AnimalManager from '../../modules/AnimalManager'
+import AnimalCard from "./AnimalCard";
+import AnimalManager from "../../modules/AnimalManager";
 
 class AnimalList extends Component {
-    //define what this component needs to render
-    state = {
-        animals: [],
-    }
+  //define what this component needs to render
+  state = {
+    animals: [],
+  };
 
-componentDidMount(){
-    console.log("ANIMAL LIST: ComponentDidMount");
+  componentDidMount() {
+    // console.log("ANIMAL LIST: ComponentDidMount");
     //getAll from AnimalManager and hang on to that data; put it in state
-    AnimalManager.getAll()
-    .then((animals) => {
-        console.log(animals)
-        this.setState({
-            animals: animals
-        })
-    })
+    AnimalManager.getAll().then((animals) => {
+      console.log(animals);
+      this.setState({
+        animals: animals,
+      });
+    });
+  }
+
+  render() {
+    // console.log("ANIMAL LIST: Render");
+
+    return (
+      <div className="container-cards">
+        {this.state.animals.map((currentAnimalInLoop) => {
+        console.log("this is a current animal in the loop", currentAnimalInLoop)
+          return <AnimalCard key={currentAnimalInLoop.id} animal={currentAnimalInLoop}/>;
+        })}
+      </div>
+    );
+  }
 }
 
-render(){
-    console.log("ANIMAL LIST: Render");
-
-    return(
-        <div className="container-cards">
-            {this.state.animals.map(animal => <AnimalCard />)}
-        </div>
-    )
-}
-}
-
-export default AnimalList
+export default AnimalList;
